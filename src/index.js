6c2fa57eda032a085aca6da57079be21174ef593
Mohamed Tamsamani <tamsmoha@gmail.com>
2019-12-13 07:18:51 +0100

    Apply Store Provider in General App Index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "./modules/store";
+import { storeReducer, initialState } from "./modules/reducer";
+
+// store functionality
+function AppIndex() {
+	const useStore = useReducer(storeReducer, initialState);
+
+	return (
+		<Provider value={useStore}>
+			<App />
+		</Provider>
+	);
+}
+
+ReactDOM.render(<AppIndex />, document.getElementById("root"));
