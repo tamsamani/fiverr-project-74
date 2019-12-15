92cc01b494186f97724667c4a7822f0baa3414f3
Mohamed Tamsamani <tamsmoha@gmail.com>
2019-12-15 04:11:00 +0100

    Update Menu & Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,18 @@
 import React from "react";
 
-import Menu from "./menu";
 import RiskLevelSelector from "./risk-level-selector";
 import InitialInvestementInput from "./InitialInvestementInput";
 import MonthlyInvestInput from "./MonthlyInvestInput";
 import FeeInput from "./FeeInput";
 import YearsSelector from "./YearsSelector";
+import { Form } from "react-bootstrap";
 
 export default props => (
-	<>
-		<Menu />
-		<div>
-			<h3>Controls:</h3>
-			<RiskLevelSelector />
-			<InitialInvestementInput />
-			<MonthlyInvestInput />
-			<YearsSelector />
-			<FeeInput />
-		</div>
-		<hr />
-	</>
+	<Form className="mt-3">
+		<RiskLevelSelector />
+		<InitialInvestementInput />
+		<MonthlyInvestInput />
+		<YearsSelector />
+		<FeeInput />
+	</Form>
 );
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button, ButtonGroup } from "react-bootstrap";
+import Header from "./Header";
 
-export default class Menu extends React.Component {
-	render() {
-		return (
-			<div>
-				<p>
-					<Link to="/table">Table</Link>
-				</p>
-				<p>
-					<Link to="/chart">Chart</Link>
-				</p>
-			</div>
-		);
-	}
-}
+export default () => {
+	const [toggle, setToggle] = useState(false);
+
+	const setToggleFx = () => setToggle(!toggle);
+
+	return (
+		<div className="mb-2">
+			<ButtonGroup>
+				<Button as={Link} to="/table" variant="outline-primary">
+					Table
+				</Button>
+				<Button as={Link} to="/chart" variant="outline-primary">
+					Chart
+				</Button>
+			</ButtonGroup>
+			<Button className="ml-3" variant={(toggle ? "" : "outline-") + "secondary"} onClick={setToggleFx}>
+				options
+			</Button>
+			{toggle && <Header />}
+		</div>
+	);
+};
